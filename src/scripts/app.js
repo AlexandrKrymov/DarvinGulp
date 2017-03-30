beb4f31104dc2d3324501fda6c55c4dc0b7e2498
berezinV <hellogoodbye2806@gmail.com>
2017-03-30 15:28:26 +0300

    app.js resize()

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,20 @@
 //= require _common/_components/forms.js
 
 
+function resize() {
+
+    if($(window).width() <= 768){
+
+    }else {
+
+    }
+}
+$(window).on('load',function () {
+    resize();
+}).on('resize',function () {
+    resize();
+}).on('click',function (e) {
+
+}).on('scroll',function () {
+
+});
