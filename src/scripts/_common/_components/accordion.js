68e0437bdf92495f4e59a433e96b04aa812c2baa
berezinV <hellogoodbye2806@gmail.com>
2017-04-04 17:14:41 +0300

    accordion.js

--- src/scripts/_common/_components/accordion.js
+++ src/scripts/_common/_components/accordion.js
@@ -1,28 +1,46 @@
-$('.js-accordion').each(function (e) {
-    var container = $(this);
-    var btn = container.find('.accordion__btn');
-    var content = container.find('.accordion__content');
-
-    btn.on('click',function (e) {
-        if(content.is(':hidden')){
-           content.stop().addClass('is-active').slideDown();
-           btn.addClass('is-active');
-        }else {
-           content.stop().removeClass('is-active').slideUp();
-            btn.removeClass('is-active');
-        }
-        e.preventDefault();
-    })
-
-});
-
-function accordionReset() {
-    $('.js-accordion').each(function (e) {
+function accordion(wrapper,screen) {
+    $(wrapper).each(function () {
         var container = $(this);
-        var btn = container.find('.accordion__btn');
-        var content = container.find('.accordion__content');
+           var btn = container.find('.accordion__btn');
+           var content = container.find('.accordion__content');
+           var breakpoint = parseInt(screen);
 
-        btn.removeClass('is-active');
-        content.removeClass('is-active').css('display','');
-    });
+           if (breakpoint){
+               btn.on('click',function (e) {
+                   if($(window).width() <= breakpoint){
+                       if(content.is(':hidden')){
+                           content.stop().addClass('is-active').slideDown();
+                           btn.addClass('is-active');
+                       }else {
+                           content.stop().removeClass('is-active').slideUp();
+                           btn.removeClass('is-active');
+                       }
+                       e.preventDefault();
+                   }
+               });
+
+               $(window).on('resize',function () {
+                   if($(window).width() > breakpoint) {
+                       btn.removeClass('is-active');
+                       content.removeClass('is-active').css('display','');
+                   }
+               });
+
+           }else{
+               btn.on('click',function (e) {
+                   if($(window).width() <= breakpoint){
+                       if(content.is(':hidden')){
+                           content.stop().addClass('is-active').slideDown();
+                           btn.addClass('is-active');
+                       }else {
+                           content.stop().removeClass('is-active').slideUp();
+                           btn.removeClass('is-active');
+                       }
+                       e.preventDefault();
+                   }
+
+               });
+           }
+       });
+
 }
