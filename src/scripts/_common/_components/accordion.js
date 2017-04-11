feb7b92cd1febe768cdd32225349432562c5809f
vberezin <hellogoodbye2806@gmail.com>
2017-04-11 23:50:00 +0300

    accordion

--- src/scripts/_common/_components/accordion.js
+++ src/scripts/_common/_components/accordion.js
@@ -1,11 +1,16 @@
 function accordion(wrapper,screen) {
+
     $(wrapper).each(function () {
-        var container = $(this);
+          var container = $(this);
            var btn = container.find('.accordion__btn');
            var content = container.find('.accordion__content');
            var breakpoint = parseInt(screen);
 
            if (breakpoint){
+               if($(window).width() <= breakpoint){
+                   container.addClass('accordion-loaded')
+               }
+
                btn.on('click',function (e) {
                    if($(window).width() <= breakpoint){
                        if(content.is(':hidden')){
@@ -23,21 +28,21 @@ function accordion(wrapper,screen) {
                    if($(window).width() > breakpoint) {
                        btn.removeClass('is-active');
                        content.removeClass('is-active').css('display','');
+                       container.removeClass('accordion-loaded')
                    }
                });
 
            }else{
+               container.addClass('accordion-loaded');
                btn.on('click',function (e) {
-                   if($(window).width() <= breakpoint){
-                       if(content.is(':hidden')){
-                           content.stop().addClass('is-active').slideDown();
-                           btn.addClass('is-active');
-                       }else {
-                           content.stop().removeClass('is-active').slideUp();
-                           btn.removeClass('is-active');
-                       }
-                       e.preventDefault();
+                   if(content.is(':hidden')){
+                       content.stop().addClass('is-active').slideDown();
+                       btn.addClass('is-active');
+                   }else {
+                       content.stop().removeClass('is-active').slideUp();
+                       btn.removeClass('is-active');
                    }
+                   e.preventDefault();
 
                });
            }
