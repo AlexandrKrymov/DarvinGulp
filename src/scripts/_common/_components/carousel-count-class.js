20bf08a6ddf6885528d828b9a8da866c54c4a916
vBerezin <berezin.vyacheslav@gmail.com>
2017-04-28 15:49:57 +0300

    Update carousel-count-class.js

--- src/scripts/_common/_components/carousel-count-class.js
+++ src/scripts/_common/_components/carousel-count-class.js
@@ -1,10 +1,21 @@
 /*
- * div.carousel.carousel_3
+ * example
+ * div.carousel.carousel_3(data-dots="false")
  *
  * */
 $('.carousel').each(function () {
     var carousel = $(this);
     var carouselLength = '';
+    var dots = true;
+    var nav = true;
+
+    if(carousel.data('dots') == false){
+        dots = false
+    }
+    if(carousel.data('nav') == false){
+        dots = false
+    }
+
     $.each(carousel.attr('class').split(/\s+/), function(i, name) {
         if (name.indexOf('carousel_') > -1) {
             carouselLength = name.match(/\d+/)[0];
@@ -12,16 +23,20 @@ $('.carousel').each(function () {
         }
     });
     if(carousel.find('.carousel__item').length > carouselLength){
+        if(carousel.hasClass('be-photoswipe-gallery')){
+            carousel.find('.element').removeClass('element');
+        }
         carousel.addClass("owl-carousel").owlCarousel({
             items:carouselLength,
-            loop:true,
-            nav:true,
-            dots:true,
-            autoplay:true,
-            autoplayTimeout:50000,
-            autoplayHoverPause:true
+            loop:false,
+            nav:nav,
+            margin:20,
+            dots:dots,
+            autoplay:false,
+            onInitialized:function () {
+                carousel.find('.owl-item').addClass('element');
+            }
         });
     }
 
 });
-
