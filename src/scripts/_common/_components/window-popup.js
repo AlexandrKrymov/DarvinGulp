1ba0da01f3b8f2293afc56e89e48ebf8f0b1ef59
vBerezin <berezin.vyacheslav@gmail.com>
2017-06-11 22:54:15 +0300

    Create window-popup.js

--- src/scripts/_common/_components/window-popup.js
+++ src/scripts/_common/_components/window-popup.js
@@ -11,31 +11,41 @@ $('.js-popup').click(function(e){
     }
 });
 
-$( ".window" ).on('click',function( event ) {
-    var popup = $(this);
-    if(event.target == $('.window__close') || event.target.closest('.window__close')){
-        try{
-            popup.remove();
-            // $("#loading").remove();
-            $(".overlay_dark").remove();
-        }
-        catch(ex)
-        {
-            alert(ex);
-        }
-        return false;
-    }else{
-        if(!(event.target.closest('.window__body'))){
+function windowClose() {
+    $( ".window" ).on('click',function( event ) {
+        var popup = $(this);
+        if(event.target == $('.window__close') || event.target.closest('.window__close')){
             try{
                 popup.remove();
                 // $("#loading").remove();
-                $(".overlay_dark").remove();
+                $("#window-overlay").remove();
             }
             catch(ex)
             {
                 alert(ex);
             }
             return false;
+        }else{
+            if(!(event.target.closest('.window__body'))){
+                try{
+                    popup.remove();
+                    // $("#loading").remove();
+                    $("#window-overlay").remove();
+                }
+                catch(ex)
+                {
+                    alert(ex);
+                }
+                return false;
+            }
         }
-    }
+    });
+}
+
+$(window).on('load',function (event) {
+    windowClose();
 });
+$(document).ajaxComplete(function() {
+    windowClose();
+});
+
