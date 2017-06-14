9f627a072d89b21f091ba2278908897ebab70456
vBerezin <berezin.vyacheslav@gmail.com>
2017-06-14 15:00:09 +0300

    Update dropdowns-select.js

--- src/scripts/_common/_components/dropdowns-select.js
+++ src/scripts/_common/_components/dropdowns-select.js
@@ -1,8 +1,13 @@
+$(window).on('click',function (e) {
+    $('.js-dropdown-select').each(function () {
+        $(this).removeClass('is-opened');
+    })
+})
 $('.js-dropdown-select').each(function () {
     var select = $(this);
     var selectDropdown = select.find('.dropdown-select__dropdown');
     var currentVal = select.find('.dropdown-select__current-val .dropdown-select__val');
-    var selectPlaceholder = select.data('select-placeholder');
+    var selectPlaceholder = select.data('placeholder');
     var selectOptions = select.find('.dropdown-select__options .dropdown-select__val');
 
     if(selectPlaceholder){
@@ -14,11 +19,26 @@ $('.js-dropdown-select').each(function () {
         currentVal = select.find('.dropdown-select__current-val .dropdown-select__val');
         selectOptions.eq(0).addClass('is-active');
     } else {
-        currentVal.text("Choose option");
+        currentVal.text("Выберите опцию");
         currentVal.attr('data-select-value', '');
     }
 
-       select.find('.dropdown-select__current-val').on('click','*',function(e) {
+
+
+    selectOptions.on('click',function () {
+        var option = $(this);
+        var clone = option.clone();
+        currentVal.replaceWith(clone);
+        currentVal = select.find('.dropdown-select__current-val .dropdown-select__val');
+        option.addClass('is-active');
+        option.siblings('.dropdown-select__val').removeClass('is-active');
+        select.removeClass('is-opened');
+        select.trigger('change');
+    });
+
+    select.find('.dropdown-select__current-val').on('click','*',function(e) {
+        $('.js-dropdown-select').not(select).removeClass('is-opened');
+
         if(select.is('.is-opened')){
             select.removeClass('is-opened');
         }else{
@@ -28,35 +48,22 @@ $('.js-dropdown-select').each(function () {
         if(event.target.closest('.dropdown-select__current-val')){
             return false;
         }
-
         e.stopPropagation();
     });
 
-    selectOptions.on('click',function () {
-       var option = $(this);
-       var clone = option.clone();
-       currentVal.replaceWith(clone);
-       currentVal = select.find('.dropdown-select__current-val .dropdown-select__val');
-       option.addClass('is-active');
-       option.siblings('.dropdown-select__val').removeClass('is-active');
-       select.removeClass('is-opened');
-       select.trigger('change');
-    });
+    /*  if(selectOptions.length > 3) {
+          selectOptions.closest('.dropdown-select__options ').mCustomScrollbar({
+              axis:'y',
+              mouseWheel:{
+                  enable: true
+              },
+              scrollbarPosition: 'inside',
+              advanced:{ updateOnContentResize: true }
+          });
+      }*/
 
-  /*  if(selectOptions.length > 3) {
-        selectOptions.closest('.dropdown-select__options ').mCustomScrollbar({
-            axis:'y',
-            mouseWheel:{
-                enable: true
-            },
-            scrollbarPosition: 'inside',
-            advanced:{ updateOnContentResize: true }
-        });
-    }*/
-
-}).on('click',function (e) {
-    e.stopPropagation();
-    $('.dropdown-select-js').not($(this)).removeClass('is-opened');
 }).on('change',function(){
-    console.log(currentVal);
-})
+    var select = $(this);
+    var currentVal = select.find('.dropdown-select__current-val .dropdown-select__val');
+    console.log(currentVal.data('select-value'));
+});
