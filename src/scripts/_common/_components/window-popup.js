ff0eb96629b148cd4ac46fda7c682dd1bd2b3492
vBerezin <berezin.vyacheslav@gmail.com>
2017-07-02 22:13:34 +0300

    Update window-popup.js

--- src/scripts/_common/_components/window-popup.js
+++ src/scripts/_common/_components/window-popup.js
@@ -3,7 +3,7 @@ $('.js-popup').click(function(e){
     var overlay = $('#js-window-overlay');
     var target = $(btn.attr('href'));
     if(target.length){
-        $('body').addClass('window-opened');
+        $('body').css('overflow','hidden');
         target.addClass('is-active').fadeIn();
         overlay.fadeIn();
         e.preventDefault();
@@ -20,5 +20,6 @@ $('.window').on('click',function( event ) {
     ){
         overlay.fadeOut();
         popup.fadeOut().removeClass('is-active');
+        $('body').css('overflow','');
     }
 });
