0eeb4f183e7508ae3361e269cbfc9d329224a882
vBerezin <berezin.vyacheslav@gmail.com>
2017-07-02 21:50:54 +0300

    Update window-popup.js

--- src/scripts/_common/_components/window-popup.js
+++ src/scripts/_common/_components/window-popup.js
@@ -1,9 +1,8 @@
 $('.js-popup').click(function(e){
     var btn = $(this);
-    var overlay = $('#window-overlay');
+    var overlay = $('#js-window-overlay');
     var target = $(btn.attr('href'));
     if(target.length){
-        $('#window-overlay').addClass('is-active');
         $('body').addClass('window-opened');
         target.addClass('is-active').fadeIn();
         overlay.fadeIn();
@@ -11,41 +10,15 @@ $('.js-popup').click(function(e){
     }
 });
 
-function windowClose() {
-    $( ".window" ).on('click',function( event ) {
-        var popup = $(this);
-        if(event.target == $('.window__close') || event.target.closest('.window__close')){
-            try{
-                popup.remove();
-                // $("#loading").remove();
-                $("#window-overlay").remove();
-            }
-            catch(ex)
-            {
-                alert(ex);
-            }
-            return false;
-        }else{
-            if(!(event.target.closest('.window__body'))){
-                try{
-                    popup.remove();
-                    // $("#loading").remove();
-                    $("#window-overlay").remove();
-                }
-                catch(ex)
-                {
-                    alert(ex);
-                }
-                return false;
-            }
-        }
-    });
-}
-
-$(window).on('load',function (event) {
-    windowClose();
+$(window).on('click',function( event ) {
+    var popup = $('.window.is-active');
+    var overlay = $('#js-window-overlay');
+    if(event.target == $('.window__close')
+        || event.target.closest('.window__close')
+        || !(event.target.closest('.window__body'))
+        || event.target == overlay
+    ){
+        overlay.fadeOut();
+        popup.fadeOut().removeClass('is-active');
+    }
 });
-$(document).ajaxComplete(function() {
-    windowClose();
-});
-
