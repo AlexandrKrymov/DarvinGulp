8a02fa55d074226ed076691a5fa95d5c477e8bd5
vBerezin <berezin.vyacheslav@gmail.com>
2017-07-04 00:16:10 +0300

    Update window-popup.js

--- src/scripts/_common/_components/window-popup.js
+++ src/scripts/_common/_components/window-popup.js
@@ -9,17 +9,25 @@ $('.js-popup').click(function(e){
         e.preventDefault();
     }
 });
+function windowCloseInit() {
+    $('body').on('click',function( event ) {
+        var popup = $('.window.is-active');
+        var overlay = $('#js-window-overlay');
+        if(event.target == $('.window__close')
+            || event.target.closest('.window__close')
+            || !(event.target.closest('.window__body'))
+            || event.target == overlay
+        ){
+            overlay.fadeOut();
+            popup.fadeOut().removeClass('is-active');
+            $('body').css('overflow','');
+        }
+    });
+}    
 
-$('.window').on('click',function( event ) {
-    var popup = $('.window.is-active');
-    var overlay = $('#js-window-overlay');
-    if(event.target == $('.window__close')
-        || event.target.closest('.window__close')
-        || !(event.target.closest('.window__body'))
-        || event.target == overlay
-    ){
-        overlay.fadeOut();
-        popup.fadeOut().removeClass('is-active');
-        $('body').css('overflow','');
-    }
+$(document).ready(function (event) {
+    windowCloseInit(event);
+});
+$(document).ajaxComplete(function (event) {
+    windowCloseInit(event)
 });
