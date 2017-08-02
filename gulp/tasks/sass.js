087e7bf6692e09f645ff1ececae03be033043491
vBerezin <berezin.vyacheslav@gmail.com>
2017-08-02 17:02:30 +0300

    Update sass.js

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -21,24 +21,6 @@ module.exports = function () {
             .pipe($.browserSync.reload({stream:true}));
     });
 
-    $.gulp.task('sass:print', function () {
-        return $.gulp.src($.config.paths.src.styles + 'print.scss')
-            .pipe($.gp.if($.dev, $.gp.sourcemaps.init()))
-            .pipe($.gp.sass({
-                    //includePaths: [$.config.paths.src.styles + 'app.scss'],
-                    includePaths: bourbon
-                }
-            ))
-            .on('error', $.gp.notify.onError({ title: 'sass' }))
-            .pipe($.gp.autoprefixer({
-                    browsers: $.config.autoprefixerConfig
-                }
-            ))
-            .pipe($.gp.if($.dev, $.gp.sourcemaps.write()))
-            .pipe($.gulp.dest($.config.paths.build.styles))
-            .pipe($.browserSync.reload({stream:true}));
-    });
-
     $.gulp.task('sass:vendor', function () {
         return $.gulp.src($.config.paths.src.styles + 'vendor.scss')
             .pipe($.gp.if($.dev, $.gp.sourcemaps.init()))
