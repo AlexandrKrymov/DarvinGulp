8316bedeb2bb75d50770dd68b4dcf8995b0c70b9
vBerezin <berezin.vyacheslav@gmail.com>
2017-08-02 17:03:58 +0300

    Update watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,7 +8,6 @@ module.exports = function () {
         $.gulp.watch(path.scriptsVendor, $.gulp.series('js:vendor'));
         $.gulp.watch(path.scriptsModerniz, $.gulp.series('js:modernizr'));
         $.gulp.watch(path.stylesApp, $.gulp.series('sass'));
-        $.gulp.watch(path.stylesPrint, $.gulp.series('sass:print'));
         $.gulp.watch(path.stylesVendor, $.gulp.series('sass:vendor'));
         $.gulp.watch(path.images, $.gulp.series('images','icons:img','sass'));
         $.gulp.watch(path.icons, $.gulp.series('icons:img','sass'));
