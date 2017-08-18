c730c75411be55fe498c126b1b1eddcd97bc3c33
vBerezin <berezin.vyacheslav@gmail.com>
2017-08-18 09:59:02 +0300

    Update textarea-line.js

--- src/scripts/_common/_components/textarea-line.js
+++ src/scripts/_common/_components/textarea-line.js
@@ -7,7 +7,7 @@ $('.js-textarea').each(function () {
         field.wrap('<div class="textarea-container" style="display: block;position: relative"></div>');
         var helper = field.clone().attr('rows', '1').css('position','fixed').css('min-height',0).css('height', field.css('line-height'));
         var container = field.closest('.textarea-container');
-        container.append('<div class="textarea-helper" style="position:absolute;left:0;top:0;width:100%;pointer-events: none;display:none;height:'+field.css('line-height')+'"></div>');
+        container.append('<div class="textarea-helper" style="position:absolute;left:0;top:'+ field.position().top +';width:100%;pointer-events: none;display:none;height:'+field.css('line-height')+'"></div>');
         var fieldActive = container.find('.textarea-helper');
 
         fieldActive.on('click', function () {
@@ -19,11 +19,13 @@ $('.js-textarea').each(function () {
             $('body').append(helper);
             autosize(helper);
             autosize.update(helper);
+            fieldActive.css('top', field.position().top);
             fieldActive.fadeIn(200);
         }).on('focusout mouseout',function () {
             if(!$.trim(helper.val()).length) {
                 fieldActive.css('height',field.css("line-height")).fadeOut(200);
             }else{
+                fieldActive.css('top', field.position().top);
                 fieldActive.fadeIn(200);
             }
             helper.remove();
